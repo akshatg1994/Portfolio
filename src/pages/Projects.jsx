28640f158414e68316bc9d1baafed1c0b27e44f3
Akshat <akshat.g1994@gmail.com>
2025-05-26 12:17:39 +0530

    Added projects section (without images)

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import projects from "../data/projects.json";
-import styles from './projects.module.css'
+import styles from "./projects.module.css";
 
 const Projects = () => {
   return (
-    <div>
+    <section>
       Projects section
       {projects.map((project, index) => (
         <RenderProject project={project} isFlipped={index % 2} />
       ))}
-    </div>
+    </section>
   );
 };
 
@@ -17,10 +17,19 @@ const RenderProject = (props) => {
   const { project, isFlipped } = props;
   return (
     <div className={styles.projectCard}>
-      <div className={styles.projectTitle}>
-
+      <div className={styles.projectTitle}>{project.name}</div>
+      <div
+        className={`${styles.projectDataHolder} ${
+          isFlipped ? styles.isFlipped : ""
+        }`}
+      >
+        <div className={styles.projectDetails}>
+          {project.description.map((desc, index) => (
+            <div>{desc}</div>
+          ))}
+          <div className={styles.projectImageWrapper}></div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
